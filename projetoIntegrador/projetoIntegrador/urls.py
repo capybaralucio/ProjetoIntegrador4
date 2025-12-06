b4747078ac6fa7e5b56f7dcc639fea5d4cc40e4a
Joaobento3 <jv3344035@gmail.com>
2025-12-06 14:47:14 -0300

    resolução conflito de merge.

--- projetoIntegrador/projetoIntegrador/urls.py
+++ projetoIntegrador/projetoIntegrador/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from entregas.views import EntregaViewSet, MotoristaViewSet, ClienteViewSet, RotaViewSet, VeiculoViewSet
+from entregas.views import (EntregaViewSet, MotoristaViewSet, ClienteViewSet, RotaViewSet, VeiculoViewSet)
 from rest_framework.authtoken.views import obtain_auth_token
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import (SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView)
+
 
 router = DefaultRouter()
 router.register(r'motorista', MotoristaViewSet)
@@ -12,14 +13,12 @@ router.register(r'rotas', RotaViewSet)
 router.register(r'veiculos', VeiculoViewSet)
 router.register(r'cliente', ClienteViewSet)
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-]
-=======
+
     path('api/', include(router.urls)),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), 
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), 
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
->>>>>>> e01effd4a5f1b08ce2084d581bac58d384f430c8
