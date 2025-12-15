ff6293d11ef56e9d8a2cccf1663ffe9faa65b30d
lucio <lucio.alvezz1829@gmail.com>
2025-12-15 20:25:05 -0300

    código admin atualizado

--- projetoIntegrador/entregas/permissions.py
+++ projetoIntegrador/entregas/permissions.py
@@ -1,6 +1,6 @@
-#professor não faou sobre isso, mas segundo o chat é necessário para o funcionamento correto das permissões
 
-from rest_framework.permissions import BasePermission #, SAFE_METHODS
+
+from rest_framework.permissions import BasePermission
 
 
 class IsAdmin(BasePermission):
@@ -51,17 +51,3 @@ class IsAnyUser(BasePermission):
                 or hasattr(user, "cliente")
             )
         )
-
-
-# class IsOwnerOrReadOnly(BasePermission):
-#     """
-#     Permissão personalizada para permitir que apenas o proprietário de um objeto
-#     possa editá-lo. Outros usuários podem apenas ler (GET, HEAD, OPTIONS).
-#     """
-#     def has_object_permission(self, request, view, obj):
-#         # Permite métodos seguros para qualquer usuário
-#         if request.method in SAFE_METHODS:
-#             return True
-        
-#         # Permite edição apenas ao proprietário do objeto
-#         return obj.owner == request.user
