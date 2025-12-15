9e156036c006d10a0a69ae58f7075d3fad52f9f4
lucio <lucio.alvezz1829@gmail.com>
2025-12-15 16:50:49 -0300

    código em admin.py

--- projetoIntegrador/entregas/admin.py
+++ projetoIntegrador/entregas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (Motorista, Veiculo, Cliente, Rota, Entrega)
+
+admin.site.register(Motorista)
+admin.site.register(Veiculo)
+admin.site.register(Cliente)
+admin.site.register(Rota)
+admin.site.register(Entrega)
 
 # Register your models here.
